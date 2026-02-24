c29dd5dc4ece778d05c6f42189db32566d328bdd
DimonK3 <johannesgawro@hotmail.com>
2026-02-24 10:35:54 +0100

    test: removed useless test

--- src/test/java/RaceCarTest.java
+++ src/test/java/RaceCarTest.java
@@ -2,8 +2,7 @@ import dxc.Race;
 import dxc.RaceCar;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RaceCarTest {
 
@@ -31,25 +30,5 @@ public class RaceCarTest {
         String actualMessage = exception.getMessage();
 
         assertTrue(actualMessage.contains(expectedMessage));
-    }
-    @Test
-    public void shouldWinAtLeastOnceInThousandRuns() {
-
-        Race race = new Race(100);
-
-        RaceCar[] cars = RaceCar.getDefaultCars();
-
-        for (RaceCar car : cars) {
-            race.addCar(car);
-        }
-
-        race.simulate(1000);
-
-        for (RaceCar car : cars) {
-            String expectedMessage = car.name + " hat in 1000 Rennen kein einziges Mal gewonnen";
-            String actualMessage = car.wins > 0 ? "OK" : expectedMessage;
-
-            assertTrue(actualMessage.contains("OK"), expectedMessage);
-        }
     }
 }
