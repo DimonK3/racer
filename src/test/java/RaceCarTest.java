51237bf76d406426f1f62223308de7c6d324e83e
DimonK3 <johannesgawro@hotmail.com>
2026-02-24 09:43:08 +0100

    test: added tests and pom.xml JUNIT

---
+++ src/test/java/RaceCarTest.java
@@ -0,0 +1,34 @@
+import dxc.RaceCar;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RaceCarTest {
+
+    @Test
+    public void shouldThrowErrorWhenDiceCountBelow1() {
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new RaceCar("car", 0, 6, 0);
+        });
+
+        String expectedMessage = "Mindestens 1 Würfel nötig";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void shouldThrowErrorWhenDiceSidesBelow1() {
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new RaceCar("car", 2, 0, 0);
+        });
+
+        String expectedMessage = "Würfel muss mindestens eine Seite haben";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+}
