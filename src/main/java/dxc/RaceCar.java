f8b6958135590876e520c5670285980ec81f2261
DimonK3 <johannesgawro@hotmail.com>
2026-02-24 09:29:54 +0100

    feat: added dice roll and reset method

--- src/main/java/dxc/RaceCar.java
+++ src/main/java/dxc/RaceCar.java
@@ -17,5 +17,25 @@ public class RaceCar {
     public RaceCar(String name, int diceCount, int diceSides, int bonus) {
         if(diceCount < 1) throw new IllegalArgumentException("Mindestens 1 Würfel nötig");
         if(diceSides < 1) throw new IllegalArgumentException("Würfel muss mindestens eine Seite haben");
+
+        this.name = name;
+        this.diceCount = diceCount;
+        this.diceSides = diceSides;
+        this.bonus = bonus;
+    }
+
+    public int roll() {
+        int sum = 0;
+        for (int i = 0; i < diceCount; i++) {
+            sum += random.nextInt(diceSides) + 1;
+        }
+
+        sum += bonus;
+
+        return Math.max(1, sum);
+    }
+
+    public void reset() {
+        position = 0;
     }
 }
